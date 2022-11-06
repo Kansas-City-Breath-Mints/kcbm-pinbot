4d4402f1f0ca85c6f2433f32e1c0bd7c866ed98a
Ryan Walker <ryan@launchcode.org>
2022-11-06 17:49:28 -0600

    .

--- main.py
+++ main.py
@@ -118,15 +118,13 @@ class Message(commands.Converter):
 #        Passel so that all pinned messages get sent to the pins archive channel.
 
 # TODO change command here if you want to use another command, replace p. with anything you want inside the single ('') quotes
-intents = discord.Intents.default()
-print(discord.__version__)
-print(intents)
-print(type(intents))
+intents = disnake.Intents.default()
 intents.message_content = True
 
 client = commands.Bot(command_prefix='p.',
                       status='Online',
-                      case_insensitive=True)
+                      case_insensitive=True,
+                      intents=intents)
 client.remove_command("help")
 
 # TODO change mode to 1 or 2 here
