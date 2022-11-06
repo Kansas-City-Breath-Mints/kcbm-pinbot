07b59d8874b9a686d7d41c57f59d4c817dde000d
Ryan Walker <ryan@launchcode.org>
2022-11-06 17:18:52 -0600

    does this work?

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ class Message(commands.Converter):
 #        Passel so that all pinned messages get sent to the pins archive channel.
 
 # TODO change command here if you want to use another command, replace p. with anything you want inside the single ('') quotes
-intents = discord.Intents.default()
+intents = discord.Intent.default()
 intents.message_content = True
 
 client = commands.Bot(command_prefix='p.',
