cce878991cf603e2497234602c7a37bc5ae0877a
Ryan Walker <ryan@launchcode.org>
2022-11-22 22:05:29 -0600

    more debug

--- main.py
+++ main.py
@@ -387,6 +387,8 @@ JUMP_LINK_MATCHER = re.compile(r"https://(?:canary|ptb)?\.?discord(?:app)?.com/c
 @client.event
 async def on_message(message):
     print(message.channel.id)
+    print(message.channel.id == 1037184519066894408)
+    print(re.search("\btictactic\b", message.content))
     if 'shrimp check' in message.content:
         await message.add_reaction("🦐")
     if message.channel.id == 1037184519066894408 and re.search("\btictactic\b", message.content):
